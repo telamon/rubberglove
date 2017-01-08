0730de96665d047ebb4a9e1d2c35822f72cb2996
Tony Ivanov <telamohn@gmail.com>
2017-01-08 17:54:29 +0100

    Added telemetry tweak

---
+++ tweaks/tweak_disable_telemetry.js
@@ -0,0 +1,18 @@
+// Tweak reference:
+// http://winaero.com/blog/how-to-disable-telemetry-and-data-collection-in-windows-10/
+
+module.exports= {
+	category: 'performance|integrity',
+	compat: 'W10+',
+	description: 'Tries to completely disable telemetry collection.',
+	up: ()=>{
+		return rubberglove.registry
+		.set('HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DataCollection','AllowTelemetry',0)
+		.then(()=>rubberglove.services.disable('dmwappushsvc'));
+	},
+	down: ()=>{
+		return rubberglove.registry
+		.unset('HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DataCollection','AllowTelemetry');
+		.then(()=>rubberglove.services.setStartup('dmwappushsvc','automatic'));
+	}
+}
